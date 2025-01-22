c7d7e46bb69ce91fc05d27d4b87a4c945e4c430d
Aayush <110086340+aayush-ak09@users.noreply.github.com>
2025-01-22 07:24:58 +0530

    Add files via upload

---
+++ src/app/MyComponent/add-todo/add-todo.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Todo } from '../../todo';
+
+@Component({
+  standalone: true,
+  selector: 'app-add-todo',
+  imports: [FormsModule],
+  templateUrl: './add-todo.component.html',
+  styleUrl: './add-todo.component.css'
+})
+export class AddTodoComponent implements OnInit {
+  title: string = '';
+  discr: string = '';
+  startTime: string = '';
+  endTime: string = '';
+  date: string = '';
+
+  @Input() editTodo!: Todo;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoEdit: EventEmitter<Todo> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.editTodo) {
+      this.title = this.editTodo.title;
+      this.discr = this.editTodo.discr;
+      this.startTime = this.editTodo.startTime;
+      this.endTime = this.editTodo.endTime;
+      this.date = this.editTodo.date;
+    }
+  }
+
+  onSubmit(): void {
+    
+    if (this.editTodo) {
+      console.log("here for editinng"+ this.editTodo)
+      const updatedTodo = {
+        ...this.editTodo,
+        title: this.title,
+        discr: this.discr,
+        startTime: this.startTime,
+        endTime: this.endTime,
+        date: this.date
+      };
+      this.todoEdit.emit(updatedTodo);
+    } else {
+      const newTodo = {
+        sno: Math.floor(Math.random() * 100),
+        title: this.title,
+        discr: this.discr,
+        active: true,
+        startTime: this.startTime,
+        endTime: this.endTime,
+        date: this.date
+      };
+      this.todoAdd.emit(newTodo);
+    }
+  }
+}
